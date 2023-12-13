ffebd68970a1a4d1beb5094e0dea77294114a56d
Naomi Lagat <naomiejerutoe@gmail.com>
2023-12-13 16:21:33 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (){
+    return 2000 / 100;
+}
+
+const square = (number) => {
+    return number * number;
+}
+
+const add = (number1, number2) => {
+    return number1 + number2;
+}
